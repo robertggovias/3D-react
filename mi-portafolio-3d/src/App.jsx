d82202bc7c79a0117408085bf0aaf99c80d1cec5
robertggovias <goviasuarez@gmail.com>
2026-04-11 22:43:51 -0300

    eror de jxx del modelo 3d del museo corregido

--- mi-portafolio-3d/src/App.jsx
+++ mi-portafolio-3d/src/App.jsx
@@ -2,18 +2,18 @@ import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 
 // 1. Importamos el componente de tu modelo (asegúrate de que el nombre coincida)
-import { Resting_zone_museum } from './Resting_zone_museum' 
+import { Model } from './Resting_zone_museum' 
 
 function App() {
   return (
-    <div style={{ height: '100vh', width: '100vw', background: '#111' }}>
+    <div style={{ height: '100vh', width: '100vw', background: '#991f1f' }}>
       <Canvas>
         <ambientLight intensity={0.7} />
         {/* Luz direccional para darle sombras al museo */}
         <directionalLight position={[10, 10, 5]} intensity={1.5} />
 
         {/* 2. Aquí llamamos a tu modelo como si fuera una etiqueta HTML */}
-        <Resting_zone_museum />
+        <Model scale={0.5} />
 
         <OrbitControls />
       </Canvas>
